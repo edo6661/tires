d7d0310763e374bccdff51450e330cb1de69840a
edawg <muhammadridho5654@gmail.com>
2025-07-24 13:07:05 +0700

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    // server: {
-    //     https: true,
-    //     host: true,
-    // },
-    // build: {
-    //     manifest: true,
-    //     outDir: 'public/build',
-    //     rollupOptions: {
-    //         output: {
-    //             manualChunks: undefined,
-    //         },
-    //     },
-    // },
 });
