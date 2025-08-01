c1f9dfd91adf446d91d22138e47a464bfb161030
edawg <muhammadridho5654@gmail.com>
2025-08-01 11:28:30 +0700

    chore: customer nav localization

--- lang/en/app.php
+++ lang/en/app.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-    'welcome' => 'Welcome to our application!',
-    'dashboard' => 'Dashboard',
-
+    'tire_installation_reservation' => 'Tire Installation Reservation',
+    'calendar' => 'Calendar',
+    'inquiry' => 'Inquiry',
+    'login' => 'Login',
+    'reservations' => 'Reservations',
+    'profile' => 'Profile',
+    'logout' => 'Logout',
 ];
--- lang/ja/app.php
+++ lang/ja/app.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
-    'welcome' => '私たちのアプリケーションへようこそ！', 
-    'dashboard' => 'ダッシュボード', 
+    'tire_installation_reservation' => 'タイヤ取付予約',
+    'calendar' => 'カレンダー',
+    'inquiry' => 'お問い合わせ',
+    'login' => 'ログイン',
+    'reservations' => '予約',
+    'profile' => 'プロフィール',
+    'logout' => 'ログアウト',
 ];
