24bd66c3d507a23af9627e7e59701c41d8081d15
Mifta Aldi <miftaadit19@gmail.com>
2025-08-21 11:41:26 +0700

    feat(API): Create annoucements translations API for admin

--- app/Http/Controllers/Api/Admin/AnnouncementController.php
+++ app/Http/Controllers/Api/Admin/AnnouncementController.php
@@ -34,11 +34,13 @@ class AnnouncementController extends Controller
         try {
             $data = $request->validated();
             $announcement = $this->announcementService->createAnnouncement($data);
-
+            foreach ($data['translations'] as $trans) {
+                $announcement->translations()->create($trans);
+            }
             return response()->json([
                 'success' => true,
                 'message' => 'Pengumuman berhasil dibuat.',
-                'data' => $announcement
+                'data' => $announcement->load('translations')
             ], 201);
         } catch (\Exception $e) {
             return response()->json([
@@ -82,6 +84,14 @@ class AnnouncementController extends Controller
                     'success' => false,
                     'message' => 'Pengumuman tidak ditemukan.'
                 ], 404);
+            }
+            if (!empty($data['translations'])) {
+                foreach ($data['translations'] as $trans) {
+                    $announcement->translations()->updateOrCreate(
+                        ['locale' => $trans['locale']], // cek per bahasa
+                        ['title' => $trans['title'], 'content' => $trans['content']]
+                    );
+                }
             }
 
             return response()->json([
